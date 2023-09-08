ed757f834a9a2b2e27d434a0e20a0589bbd1905f
Kaitlin Thornton <thorntonkaitlin58@yahoo.com>
2023-09-07 18:00:34 -0600

    Update scripts.js
    Deleted previous exercise information and replaced with pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-alert('hello world');
-let favoritefood = 'chocolate cake';
-document.write(favoritefood);
+const pokemonList= [
+    {name: 'Bulbasaur',
+     height: 0.7, 
+     types: ['grass', 'poison']},
+    {name: 'Charmander', 
+    height: 0.6, 
+    types: ['fire']},
+    {name: 'Squirtle', 
+    height: 0.5, 
+    types: ['water']},
+]
+
+
+const apiurl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
